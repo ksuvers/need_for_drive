5751defca87d598585a1c7e71715f2a03c72a6f6
ksuvers <ksulivigo@gmail.com>
2021-07-16 21:54:06 +0300

    add main page

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,27 @@
 $(function() {
-    $('sidebar__line').on('click', function() {
-        $('main__menu').slideToggle();
+    $('.side-menu__line').on('click', function() {
+        $('.main__menu').slideToggle();
     });
-
-    const swiper = new Swiper('.swiper-container', {
-        direction: 'vertical',
-        loop: true,
-
-        pagination: {
-            el: '.swiper-pagination',
-        },
-
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
+    $('.main__btn').on('click', function() {
+        $('.main__menu').slideToggle();
     });
+    /* document.querySelector('#menu").classList.add('menu--active'); */
+});
+
+
+new Swiper('.main__slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    loop: true,
+    speed: 800,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
 });
